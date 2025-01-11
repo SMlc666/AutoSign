3075837065a22995533d6ec0a0f6af9d7074ca8f
SMlc666 <qq1030452538@outlook.com>
2025-01-11 17:52:37 +0800

    fix(down.sh): 移除多余的安卓应用下载和提取步骤

--- down.sh
+++ down.sh
@@ -9,14 +9,3 @@ android_link=$(grep -oP 'android_link\s*=\s*android_type\s*\?\s*"\K[^"]+' script
 echo "Extracted Android download link: $android_link"
 wget -O $workDir/android.apk $android_link
 echo "Download of Android app completed!"
-7zz x $workDir/android.apk -o$workDir/android_app
-echo "Extracting Android app completed!"
-cp $workDir/android_app/lib/$AppArch/libminecraftpe.so $workDir/libminecraftpe.so
-echo "Copying libminecraftpe.so completed!"
-rm -rf $workDir/script.js $workDir/android.apk $workDir/android_app
-xmake f -p linux -a $BuildArch -m release --toolchain=clang-18 
-xmake
-echo "Building completed!"
-cp build/linux/$BuildArch/release/AutoSign $workDir/AutoSign
-chmod 777 $workDir/AutoSign
-./AutoSign
