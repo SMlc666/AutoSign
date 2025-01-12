92b8c0bcda5dbd301f6e814cc552ad27fa7b12c2
SMlc666 <qq1030452538@outlook.com>
2025-01-12 10:10:04 +0800

    feat(core): 添加`makeSignature`函数用于生成签名并优化模式搜索逻辑

--- main.cpp
+++ main.cpp
@@ -16,6 +16,35 @@ std::unordered_map<std::string, std::string> IDApattern = {
      "? ? ? D1 ? ? ? 6D ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 "
      "? ? ? 91 ? ? ? D5 ? ? ? F9 F4 03 00 AA ? ? ? F8 ? ? ? 95"},
 };
+qis::signature makeSignature(const std::string &pattern) {
+    std::string mask;
+    std::vector<char> bytes;
+
+    const size_t pattern_len = pattern.length();
+    for (std::size_t i = 0; i < pattern_len; i++) {
+        if (pattern[i] == ' ')
+            continue;
+
+        if (pattern[i] == '?') {
+            bytes.push_back(0);
+            mask += '?';
+        } else if (pattern_len > i + 1 && std::isxdigit(pattern[i]) && std::isxdigit(pattern[i + 1])) {
+            bytes.push_back(static_cast<char>(std::stoi(pattern.substr(i, 2), nullptr, 16)));
+            mask += 'x';
+            i++; // Skip the next character since we processed two characters
+        }
+    }
+
+    if (bytes.empty() || mask.empty() || bytes.size() != mask.size()) {
+        throw std::invalid_argument("Invalid pattern");
+    }
+
+    // Convert bytes to a string_view
+    std::string_view bytes_view(bytes.data(), bytes.size());
+    std::string_view mask_view(mask);
+
+    return qis::signature(bytes_view, mask_view);
+}
 int main() {
   std::vector<std::uint8_t> memory;
   const std::string pathfile = "libminecraftpe.so";
@@ -26,7 +55,7 @@ int main() {
   file.close();
   for (auto &pattern : IDApattern) {
     std::cout << "Searching for " << pattern.first << std::endl;
-    const qis::signature search(pattern.second);
+    const qis::signature search = makeSignature(pattern.second);
     const auto pos = qis::scan(memory.data(), memory.size(), search);
     if (pos == qis::npos) {
       std::cout << "Not found" << std::endl;
